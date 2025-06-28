c74c506bd2fc9612987a68ba4a84b7cbb413c2af
MARGARETA BUNGA KELORE <margaretabungakelore@gmail.com>
2025-06-28 14:50:12 +0800

    Add files via upload

---
+++ UAS/src/uas/UAS.java
@@ -0,0 +1,18 @@
+
+package uas;
+
+/**
+ *
+ * @author MARGARETA BUNGA KELORE
+ * TGL 28 JUNI 2025
+ */
+public class UAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
